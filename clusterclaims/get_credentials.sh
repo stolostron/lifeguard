2a132f8c0fb900641337d7f893fe2c181372d379
Kevin Cormier <kcormier@redhat.com>
2021-02-03 20:33:37 -0500

    Make oc-login.sh executable

--- clusterclaims/get_credentials.sh
+++ clusterclaims/get_credentials.sh
@@ -192,5 +192,6 @@ echo $password > $CLUSTERCLAIM_NAME/kubeadmin-password
 oc get secret -n $CC_NS $kubeconfig_secret -o json | jq -r '.data.kubeconfig' | base64 --decode > $CLUSTERCLAIM_NAME/kubeconfig
 echo "#!/bin/bash
 oc login $api_url -u $username -p $password --insecure-skip-tls-verify=true" > $CLUSTERCLAIM_NAME/oc-login.sh
+chmod +x $CLUSTERCLAIM_NAME/oc-login.sh
 
 printf "${GREEN}Cluster credentials extracted for ${CC_NS}.  You can find full credentials in '$PWD/$CLUSTERCLAIM_NAME'.${CLEAR}\n"
