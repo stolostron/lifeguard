814ea7db5800400c2fa4eb7fbbce2740da248b3f
Kevin Cormier <kcormier@redhat.com>
2021-03-08 10:56:23 -0500

    Use clusterclaim.hive to get correct CRD

--- clusterclaims/get_credentials.sh
+++ clusterclaims/get_credentials.sh
@@ -90,7 +90,7 @@ printf "${GREEN}* Using $CLUSTERPOOL_TARGET_NAMESPACE\n${CLEAR}"
 #----SELECT A CLUSTERCLAIM TO EXTRACT CREDENTIALS FROM----#
 if [[ "$CLUSTERCLAIM_NAME" == "" ]]; then
     # Prompt the user to choose a ClusterImageSet
-    clusterclaims=$(oc get clusterclaim -n ${CLUSTERPOOL_TARGET_NAMESPACE})
+    clusterclaims=$(oc get clusterclaim.hive -n ${CLUSTERPOOL_TARGET_NAMESPACE})
     clusterclaim_names=()
     i=0
     IFS=$'\n'
@@ -107,7 +107,7 @@ if [[ "$CLUSTERCLAIM_NAME" == "" ]]; then
         i=$((i+1))
     done;
     if [[ "$i" -lt 1 ]]; then
-        errorf "${RED}No ClusterClaims found in the ${CLUSTERPOOL_TARGET_NAMESPACE} namespace on ${HOST_URL}.  Please verify that ${CLUSTERPOOL_TARGET_NAMESPACE} has ClusterClaims with 'oc get clusterclaim -n $CLUSTERPOOL_TARGET_NAMESPACE' and try again.${CLEAR}\n"
+        errorf "${RED}No ClusterClaims found in the ${CLUSTERPOOL_TARGET_NAMESPACE} namespace on ${HOST_URL}.  Please verify that ${CLUSTERPOOL_TARGET_NAMESPACE} has ClusterClaims with 'oc get clusterclaim.hive -n $CLUSTERPOOL_TARGET_NAMESPACE' and try again.${CLEAR}\n"
         exit 3
     fi
     unset IFS
@@ -123,7 +123,7 @@ if [[ "$CLUSTERCLAIM_NAME" == "" ]]; then
 else
     oc get clusterclaim ${CLUSTERCLAIM_NAME} --no-headers &> /dev/null
     if [[ $? -ne 0 ]]; then
-        errorf "${RED}Couldn't find a ClusterClaim named ${CLUSTERCLAIM_NAME} on ${HOST_URL} in the ${CLUSTERPOOL_TARGET_NAMESPACE} namespace, validate your choice with 'oc get clusterclaim -n ${CLUSTERPOOL_TARGET_NAMESPACE}' and try again.${CLEAR}\n"
+        errorf "${RED}Couldn't find a ClusterClaim named ${CLUSTERCLAIM_NAME} on ${HOST_URL} in the ${CLUSTERPOOL_TARGET_NAMESPACE} namespace, validate your choice with 'oc get clusterclaim.hive -n ${CLUSTERPOOL_TARGET_NAMESPACE}' and try again.${CLEAR}\n"
         exit 3
     fi
 fi
@@ -142,7 +142,7 @@ fi
 # Initialize loop variables
 CC_JSON=$CLUSTERCLAIM_NAME/.ClusterClaim.json
 CD_JSON=$CLUSTERCLAIM_NAME/.ClusterDeployment.json
-oc get clusterclaim ${CLUSTERCLAIM_NAME} -n ${CLUSTERPOOL_TARGET_NAMESPACE} -o json > $CC_JSON
+oc get clusterclaim.hive ${CLUSTERCLAIM_NAME} -n ${CLUSTERPOOL_TARGET_NAMESPACE} -o json > $CC_JSON
 CC_NS=`jq -r '.spec.namespace' $CC_JSON`
 if [[ "$CC_NS" != "null" ]]; then
     oc get clusterdeployment $CC_NS -n $CC_NS -o json > $CD_JSON
